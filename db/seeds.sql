efeda6d002ba4ef0084de70dd46a3ee070de948d
Ehren Davis <ehren.davis12@gmail.com>
2018-11-05 10:02:23 -0700

    init the project using another project as its template

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (name) VALUES ('doubble Man');
+INSERT INTO burgers (name) VALUES ('tripple bacon');
+INSERT INTO burgers (name, devoured) VALUES ('smelly sauce', true);
+INSERT INTO burgers (name, devoured) VALUES ('pumpkein spiced meat', true);
+INSERT INTO burgers (name, devoured) VALUES ('Jim Bean Jack Daniels', true);
+INSERT INTO burgers (name) VALUES ('blue chees');
